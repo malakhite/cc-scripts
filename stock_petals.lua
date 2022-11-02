9f0357204b219df15d84ebd8c299e886bf5ed724
Scott Abbey <scott@scottabbey.com>
2022-11-01 21:06:11 -0400

    add stock_petals

---
+++ stock_petals.lua
@@ -0,0 +1,19 @@
+local modem = peripheral.find("modem")
+local turtleName = modem.getNameLocal()
+print("turtle: ", turtleName)
+
+local names = modem.getNamesRemote()
+print("network names: " textutils.serialize(names))
+
+-- function grow(slot)
+-- 	inter.pushItems(turtleName, 1, 1, 1)
+-- 	drawers.pushItems(turtleName, slot, 1, 2)
+-- 	turtle.select(2)
+-- 	turtle.place()
+-- 	turtle.select(1)
+-- 	turtle.place()
+-- 	turtle.dig()
+-- 	turtle.craft()
+-- end
+
+modem.closeAll()
