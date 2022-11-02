7b3e3a806d33c078e0856a2bfadd6ba72ddb7676
Scott Abbey <scott@scottabbey.com>
2022-11-01 21:29:23 -0400

    Debug remote methods

--- stock_petals.lua
+++ stock_petals.lua
@@ -1,11 +1,14 @@
 local modem = peripheral.find("modem")
 local turtleName = modem.getNameLocal()
-local inter = peripheral.find("ae2:interface_1")
-local drawers = peripheral.find("functionalstorage:storage_controller_1")
+local inter = "ae2:interface_1"
+local drawers = "functionalstorage:storage_controller_1"
+
+print(textutils.serialise(modem.getMethodsRemote(inter)))
+print(textutils.serialise(modem.getMethodsRemote(drawers)))
 
 function grow(slot)
-	inter.pushItems(turtleName, 1, 1, 1)
-	drawers.pushItems(turtleName, slot, 1, 2)
+	modem.callRemote(inter, pushItems, turtleName, 1, 1, 1)
+	modem.callRemote(drawers, pushItems, turtleName, slot, 1, 2)
 	turtle.select(2)
 	turtle.place()
 	turtle.select(1)
@@ -14,4 +17,4 @@ function grow(slot)
 	turtle.craft()
 end
 
-inter.list()
+grow(1)
