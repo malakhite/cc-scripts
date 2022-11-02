23d2d6c9ccd537c78c2e4f44752090243290595b
Scott Abbey <scott@scottabbey.com>
2022-11-01 21:38:59 -0400

    Add error handling for craft

--- stock_petals.lua
+++ stock_petals.lua
@@ -3,18 +3,18 @@ local turtleName = modem.getNameLocal()
 local inter = "ae2:interface_1"
 local drawers = "functionalstorage:storage_controller_1"
 
-print(textutils.serialise(modem.getMethodsRemote(inter)))
-print(textutils.serialise(modem.getMethodsRemote(drawers)))
-
 function grow(slot)
-	modem.callRemote(inter, pushItems, turtleName, 1, 1, 1)
-	modem.callRemote(drawers, pushItems, turtleName, slot, 1, 2)
+	modem.callRemote(inter, "pushItems", turtleName, 1, 1, 1)
+	modem.callRemote(drawers, "pushItems", turtleName, slot, 1, 2)
 	turtle.select(2)
 	turtle.place()
 	turtle.select(1)
 	turtle.place()
-	turtle.dig()
-	turtle.craft()
+	turtle.dig("right")
+	local success, reason = turtle.craft()
+	if not success then
+		print("Unable to craft: ", reason)
+	end
 end
 
 grow(1)
