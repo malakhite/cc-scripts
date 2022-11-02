e7c6f6a211265fd8aec46fb02fde7ffe1f6ffcdb
Scott Abbey <scott@scottabbey.com>
2022-11-01 21:13:07 -0400

    Add other peripherals

--- stock_petals.lua
+++ stock_petals.lua
@@ -1,19 +1,17 @@
 local modem = peripheral.find("modem")
 local turtleName = modem.getNameLocal()
-print("turtle: ", turtleName)
+local inter = peripheral.find("ae2:interface_1")
+local drawers = peripheral.find("functionalstorage:storage_controller_1")
 
-local names = modem.getNamesRemote()
-print("network names: ", textutils.serialize(names))
+function grow(slot)
+	inter.pushItems(turtleName, 1, 1, 1)
+	drawers.pushItems(turtleName, slot, 1, 2)
+	turtle.select(2)
+	turtle.place()
+	turtle.select(1)
+	turtle.place()
+	turtle.dig()
+	turtle.craft()
+end
 
--- function grow(slot)
--- 	inter.pushItems(turtleName, 1, 1, 1)
--- 	drawers.pushItems(turtleName, slot, 1, 2)
--- 	turtle.select(2)
--- 	turtle.place()
--- 	turtle.select(1)
--- 	turtle.place()
--- 	turtle.dig()
--- 	turtle.craft()
--- end
-
-modem.closeAll()
+grow(1)
